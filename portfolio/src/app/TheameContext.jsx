8e7cf8ae4190db989f389d5e3bd4c050f887a7cc
fallowShades <fallowshades@gmail.com>
2024-02-12 13:36:55 +0100

    default and toggle background color

--- portfolio/src/app/TheameContext.jsx
+++ portfolio/src/app/TheameContext.jsx
@@ -1,10 +1,15 @@
 'use client'
 import React, { createContext, useContext, useReducer } from 'react'
 import { Navbar } from '@/components'
+import { useEffect } from 'react'
 const ThemeContext = createContext()
 
+const getInitialDarkMode = () => {
+  return 'dark'
+}
+
 const initialState = {
-  theme: 'light',
+  theme: getInitialDarkMode() || 'light',
   userPreferences: {
     fontSize: 'medium',
     reduceAnimations: false,
@@ -13,18 +18,78 @@ const initialState = {
 
 function themeReducer(state, action) {
   // your code here
-}
-
-const setTheme = (theme) => {
-  console.log('change')
+  switch (action.type) {
+    case 'TOGGLE_THEME':
+      return {
+        ...state,
+        theme: action.theme,
+      }
+    case 'SET_FONT_SIZE':
+      return {
+        ...state,
+        userPreferences: {
+          ...state.userPreferences,
+          fontSize: action.payload,
+        },
+      }
+    case 'TOGGLE_REDUCED_MOTION':
+      return {
+        ...state,
+        userPreferences: {
+          ...state.userPreferences,
+          reduceAnimations: !state.userPreferences.reduceAnimations,
+        },
+      }
+    default:
+      return state
+  }
 }
 
 const ThemeProvider = ({ children }) => {
-  // your code here
+  const [state, dispatch] = useReducer(themeReducer, initialState)
 
+  useEffect(() => {
+    document.documentElement.classList.remove(
+      ...document.documentElement.classList
+    )
+    if (state.theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    }
+    if (state.theme === 'light') {
+      document.documentElement.classList.add('light')
+    }
+    if (state.theme === 'system') {
+      document.documentElement.classList.add('bg-red-200')
+    }
+  }, [state.theme])
+
+  const setTheme = (theme) => {
+    // your code here
+    switch (theme) {
+      case 'light':
+        dispatch({
+          type: 'TOGGLE_THEME',
+          theme: 'light',
+        })
+        break
+      case 'dark':
+        dispatch({
+          type: 'TOGGLE_THEME',
+          theme: 'dark',
+        })
+        break
+      case 'system':
+        dispatch({
+          type: 'TOGGLE_THEME',
+          theme: 'red-200',
+        })
+        break
+    }
+  }
+  console.log(state.theme)
   return (
     <ThemeContext.Provider value={{ setTheme }}>
-      <main className="">
+      <main className={`bg-${state.theme}`}>
         <Navbar />
         <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
       </main>

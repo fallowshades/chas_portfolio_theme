82ee0a055a5800d209f094dc0c3e2af36e244f8f
fallowShades <fallowshades@gmail.com>
2024-02-12 14:54:04 +0100

    import cards from shadcn

---
+++ portfolio/src/components/ProjectCard.jsx
@@ -0,0 +1,17 @@
+'use client'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+import { Separator } from './ui/separator'
+import { Button } from './ui/button'
+import { Badge } from './ui/badge'
+const ProjectCard = () => {
+  return <div>ProjectCard</div>
+}
+export default ProjectCard
--- portfolio/src/components/ProjectContainer.jsx
+++ portfolio/src/components/ProjectContainer.jsx
@@ -1,4 +1,7 @@
+'use client'
+import { projects } from '@/data/data'
+import ProjectCard from './ProjectCard.jsx'
 const ProjectContainer = () => {
-  return <div>ProjectContainer</div>
+  return <ProjectCard />
 }
 export default ProjectContainer
